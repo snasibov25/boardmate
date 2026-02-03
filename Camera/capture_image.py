b484074e39e8875010c199ab320dde9730d9797c
Ellaxyj <s2563745@ed.ac.uk>
2026-02-03 10:13:59 +0000

    Camera

---
+++ Camera/capture_image.py
@@ -0,0 +1,12 @@
+## libcamera-still —nopreview -o test.jpg                       (insturction for taking picture)
+## gpicview test_().jpg                                         (show pic)
+## scp pi@(raspberry's name):/home/pi/test_().jpg ~/Desktop/    (copying image to desktop on DICE)
+
+import os
+from time import sleep
+
+for i in range(5):
+    sleep(1)
+    os.system("libcamera-still --nopreview -o test_{0}.jpg".format(i))
+    
+    
