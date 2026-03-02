0b9d736099525a2973e2a2e754568b0ef698129f
Ellaxyj <s2563745@ed.ac.uk>
2026-03-02 15:33:01 +0000

    update

--- Camera/capture_image.py
+++ Camera/capture_image.py
@@ -1,9 +1,17 @@
 from pathlib import Path
-import os
 from time import sleep
+import subprocess
+BASE_DIR = Path(__file__).resolve().parent
 
-for i in range(5):
-    os.system("scp pi@snorlax:/home/pi/test{0}.jpg ~/Desktop/captures".format(i))
+
+LOCAL_DIR = BASE_DIR / "Image"
+LOCAL_DIR.mkdir(parents=True, exist_ok=True)
+
+REMOTE_USER_HOST = "pi@snorlax"
+REMOTE_DIR = "/home/pi"
+NUM_IMAGES = 6
+
+for i in range(NUM_IMAGES):
+    remote_file = f"{REMOTE_USER_HOST}:{REMOTE_DIR}/test{i}.jpg"
+    subprocess.run(["scp", remote_file, str(LOCAL_DIR)], check=True)
     sleep(1)
-    if i >= 5:
-        break
